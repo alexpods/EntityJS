a94b40261a54671ab16a74d0a6ccb243d993bd09
alexpods <aleksey.podskrebyshev@gmail.com>
2013-09-22 02:43:16 +0400

    Configure Gruntfile.js. Change progect name to EntityJS

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,4 +2,21 @@
 
 module.exports = function(grunt) {
 
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        concat: {
+            EntityManagerJS: {
+                dest: "build/entity.js",
+                src: [
+                    "src/.prefix",
+                    "src/manager.js",
+                    "src/.suffix"
+                ]
+            }
+        }
+    })
+
+    grunt.registerTask('default', ['concat']);
 };
---
+++ src/manager.js
@@ -0,0 +1,24 @@
+var Manager = {
+
+    createClass: function(meta) {
+
+    }
+};
+
+Entity.Manager.createClass('Hello', {
+    consts: {
+
+    },
+    class_props: {
+
+    },
+    class_methods: {
+
+    },
+    props: {
+
+    },
+    methods: {
+
+    }
+})
